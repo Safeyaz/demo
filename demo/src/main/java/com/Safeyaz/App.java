52232ea8d8160215a42ee4cdf37644cff6c9fa7c
unknown <safeyazawath@gmail.com>
2025-03-13 15:32:24 +0530

    project 8

---
+++ demo/src/main/java/com/Safeyaz/App.java
@@ -0,0 +1,13 @@
+package com.Safeyaz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
